5f90b6db1f0d0f59ec8bf10cd0811adc233c0c80
Newton Ongeri <ongerinewton10@gmail.com>
2026-02-22 11:27:12 +0300

    setup rust and run rust cli app

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    println!("Enter your name:");
+
+    let mut name = String::new();
+
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read input");
+
+    println!("Hello, {}! Welcome to Rust ", name.trim());
+}
